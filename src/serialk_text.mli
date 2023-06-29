95139cfef2df2f5d4808f51a7216eeb66e347088
Daniel Bünzli <daniel.buenzli@erratique.ch>
2023-06-30 01:31:53 +0200

    Spdxify.

--- src/serialk_text.mli
+++ src/serialk_text.mli
@@ -1,6 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2019 The b0 programmers. All rights reserved.
-   Distributed under the ISC license, see terms at the end of the file.
+   SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
 (** UTF-8 text lexing tools.
@@ -334,19 +334,3 @@ module Tdec : sig
       byte to the lexeme. {b Warning.} {!accept_byte}'s warning
       applies. *)
 end
-
-(*---------------------------------------------------------------------------
-   Copyright (c) 2019 The b0 programmers
-
-   Permission to use, copy, modify, and/or distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-  ---------------------------------------------------------------------------*)
